0637b717fcce26693f0fed53923ffc09d2c7af4d
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-08 10:04:42 -0400

    Math and Comparison assignment

---
+++ C-sharp-challenges/C-sharp-challenges/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C_sharp_challenges
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int num1 = 15;
+            //int num2 = 5;
+
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+
+            //int roomTemp = 70;
+            //int currentTemp = 70;
+            //bool isWarm = currentTemp >= roomTemp;
+            //Console.WriteLine(isWarm);
+
+            string userMulti = Console.ReadLine();
+            int multi = Convert.ToInt32(userMulti);
+            Console.WriteLine(multi * 50);
+
+            string userAdd = Console.ReadLine();
+            int add = Convert.ToInt32(userAdd);
+            Console.WriteLine(add + 25);
+
+            string userDevide = Console.ReadLine();
+            double devide = Convert.ToDouble(userDevide);
+            Console.WriteLine(devide / 12.5);
+
+            string userCompare = Console.ReadLine();
+            int compare = Convert.ToInt32(userCompare);
+            Console.WriteLine(compare > 50);
+
+            string userRemainder = Console.ReadLine();
+            int remainder = Convert.ToInt32(userRemainder);
+            Console.WriteLine(remainder % 7);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Math-and-Comparison-assignment/Math-and-Comparison-assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Math_and_Comparison_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+
+            Console.WriteLine("Hourly rate?");
+            string person1HourlyRate = Console.ReadLine();
+            int person1Rate = Convert.ToInt32(person1HourlyRate);
+            Console.WriteLine("Hours worked per week?");
+            string person1HoursWorked = Console.ReadLine();
+            int person1Hours = Convert.ToInt32(person1HoursWorked);
+
+            Console.WriteLine("Person 2");
+
+            Console.WriteLine("Hourly rate?");
+            string person2HourlyRate = Console.ReadLine();
+            int person2Rate = Convert.ToInt32(person2HourlyRate);
+            Console.WriteLine("Hours worked per week?");
+            string person2HoursWorked = Console.ReadLine();
+            int person2Hours = Convert.ToInt32(person2HoursWorked);
+
+            Console.WriteLine("Annual salary of Person 1:");
+            int person1TotalHours = person1Hours * 52;
+            int person1AnnualSalary = person1TotalHours * person1Rate;
+            Console.WriteLine(person1AnnualSalary);
+
+            Console.WriteLine("Annual salary of Person 2:");
+            int person2TotalHours = person2Hours * 52;
+            int person2AnnualSalary = person2TotalHours * person2Rate;
+            Console.WriteLine(person2AnnualSalary);
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool moneyCompare = person1AnnualSalary > person2AnnualSalary;
+            Console.WriteLine(moneyCompare);
+            Console.ReadLine();
+        }
+    }
+}
