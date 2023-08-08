5621b8e153d8dbcef89435cd0b1133ccfee5c46b
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-08 09:09:24 -0400

    Catching up

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //A simple  program
+        //Console.WriteLine("What is your name?");
+        //string yourName = Console.ReadLine();
+        //Console.WriteLine("Your name is: " + yourName);
+        //Console.ReadLine();
+
+        //Console.WriteLine("What is your favorite number?");
+        //string favNum = Console.ReadLine();
+        //int favoriteNum = Convert.ToInt32(favNum);
+        //int total = favoriteNum + 5;
+        //Console.WriteLine("Your favorite number + 5 is: " + total);
+        //Console.ReadLine();
+    }
+}
+
