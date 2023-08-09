dd5c4beb3899f9871e988364a9c47080b10c9c2c
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-09 09:19:07 -0400

    Boolean Assignment

---
+++ Boolean-logic-assignment/Boolean-logic-assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Boolean_logic_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string ageInYears = Console.ReadLine();
+            int age = Convert.ToInt32(ageInYears);
+
+            Console.WriteLine("Have you sever had a DUI?");
+            string everHadDUI = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(everHadDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string numOfSpeedingTickets = Console.ReadLine();
+            int numTickets = Convert.ToInt32(numOfSpeedingTickets);
+
+            Console.WriteLine("Qualified for car insurence?");
+            bool qualified = age > 15 && DUI == false && numTickets < 3;
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+        }
+    }
+}
