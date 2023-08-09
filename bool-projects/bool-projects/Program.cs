78a6f625cdbae8c4f887c7d1a12e21c226463e1c
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-09 09:08:09 -0400

    Basic boolean logic

---
+++ bool-projects/bool-projects/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bool_projects
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(true ^ false);
+            Console.ReadLine();
+        }
+    }
+}
