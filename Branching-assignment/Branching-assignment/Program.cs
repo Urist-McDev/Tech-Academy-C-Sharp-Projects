d11adf6c635d7460d28c97741e1a49659c48c43a
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-10 09:45:02 -0400

    Branching assignment

---
+++ Branching-assignment/Branching-assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Branching_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Package weight in pounds?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("This package is too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Package width in inches.");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Package height in inches.");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Package length in inches.");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            if (width + height + length > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+            }
+            else
+            {
+                decimal quote = (height * width * length) * weight / 100;
+                Console.WriteLine("Your quote for total shipping cost is: $" + quote);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main()
+        {
+            int roomTemp = 70;
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temp where you are?");
+            int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemp == roomTemp)
+            {
+                Console.WriteLine("It is exactly room temp.");
+            }
+            else if (currentTemp > roomTemp)
+            {
+                Console.WriteLine("It is warmer than room temp");
+            }
+            else if (roomTemp > currentTemp)
+            {
+                Console.WriteLine("It is colder than room temp");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+
+            Console.ReadLine();
+
+            //int currentTemp = 80;
+            //int roomTemp = 70;
+
+            //string comparisonResult = currentTemp == roomTemp ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+
+            ////if (currentTemp == roomTemp)
+            ////{
+            ////    Console.WriteLine("It is room temp right now.");
+            ////}
+            ////else if (currentTemp > roomTemp)
+            ////{
+            ////    Console.WriteLine("It is warmer than room temp.");
+            ////}
+            ////else if (currentTemp < roomTemp)
+            ////{
+            ////    Console.WriteLine("Room temp is warmer than current temp.");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("It is not room temp right now.");
+            ////}
+            //Console.ReadLine();
+        }
+    }
+}
