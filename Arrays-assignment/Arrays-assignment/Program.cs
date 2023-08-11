80d98869c15ccae38849b13675276d3a915f28a4
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-11 11:15:25 -0400

    Arrays and lists

---
+++ Arrays-assignment/Arrays-assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] cakes = { "Vanilla", "Chocolate", "Marbled", "Red Velvet" };
+            int[] intArray = { 4, 6, 9, 541, 67842 };
+
+            Console.WriteLine("For a string, pick a number between 0 and 3");
+            int stringChoice = Convert.ToInt32(Console.ReadLine());
+            if (stringChoice >= cakes.Length)
+            {
+                Console.WriteLine("Your choice must be bewteen 0 and 3.");
+            }
+            else
+            {
+                Console.WriteLine(cakes[stringChoice]);
+            }
+
+            Console.WriteLine("For a interger, pick a number between 0 and 4");
+            int intChoice = Convert.ToInt32(Console.ReadLine());
+            if (intChoice >= intArray.Length)
+            {
+                Console.WriteLine("Your choice must be bewteen 0 and 4.");
+            }
+            else
+            {
+                Console.WriteLine(intArray[intChoice]);
+            }
+
+            List<string> weapons = new List<string>();
+            weapons.Add("Axe");
+            weapons.Add("Sword");
+            weapons.Add("Bow");
+            weapons.Add("Spear");
+
+            Console.WriteLine("For a weapon, pick a number between 0 and 3");
+            int weaponChoice = Convert.ToInt32(Console.ReadLine());
+            if (weaponChoice >= weapons.Count)
+            {
+                Console.WriteLine("Your choice must be bewteen 0 and 3");
+            }
+            else
+            {
+                Console.WriteLine(weapons[weaponChoice]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ strings/strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string part1 = "This ";
+            string part2 = "is built ";
+            string part3 = "from three parts.";
+
+            string upperCase = "this string will be in uppercase.";
+
+            Console.WriteLine(part1 + part2 + part3);
+            Console.WriteLine(upperCase.ToUpper());
+
+            Console.ReadLine();
+        }
+    }
+}
