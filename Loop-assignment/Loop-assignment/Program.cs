db964bd3fcd27dc056ea9854e877a4072969bd70
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-11 10:19:06 -0400

    Loop assignment

---
+++ Loop-assignment/Loop-assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loop_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isDoWhile = false;
+            bool isWhile = true;
+
+            do
+            {
+                Console.WriteLine("This is a do while loop.");
+            }
+            while (isDoWhile == true);
+
+            while (isWhile == true)
+            {
+                Console.WriteLine("This is a while loop.");
+            }
+        }
+    }
+}
