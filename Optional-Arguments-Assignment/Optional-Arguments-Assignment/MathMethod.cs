c9648eeb419126a3ada9c5e21dc08dc90c40e4e9
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-15 11:26:47 -0400

    Optional Arguments Assignment

---
+++ Optional-Arguments-Assignment/Optional-Arguments-Assignment/MathMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Arguments_Assignment
+{
+    class MathMethod
+    {
+        public static int Math(int num1, int? num2 = 1)
+        {
+            return (int)(num1 + num2);
+        }
+    }
+}
---
+++ Optional-Arguments-Assignment/Optional-Arguments-Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Arguments_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number. After, you can enter a second number as well, but it is not needed.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int? num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum is: " + MathMethod.Math(num1, num2));
+
+            Console.ReadLine();
+        }
+    }
+}
