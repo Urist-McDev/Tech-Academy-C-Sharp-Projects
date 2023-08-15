3ee1cc4ba4e956daac64d7c32a9d35481d929ad9
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-15 11:33:17 -0400

    Void Methods assignment

---
+++ void-method-assignment/void-method-assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_method_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMath.Add(7, 8);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ void-method-assignment/void-method-assignment/voidMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_method_assignment
+{
+    class voidMath
+    {
+        public static void Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+
+            Console.WriteLine(num2);
+        }
+    }
+}
