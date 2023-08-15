975407f52921d55427329987fe78166856c5082c
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-15 10:23:49 -0400

    Blackjack work and Calling Methods assignment

---
+++ Calling-Methods-Assignment/Calling-Methods-Assignment/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Math
+    {
+        public static int Add(int num)
+        {
+           int sum = num + 5;
+           return sum;
+        }
+
+        public static int Subtract(int num)
+        {
+            int sum = num - 5;
+            return sum;
+        }
+
+        public static int Multiply(int num)
+        {
+            int sum = num * 5;
+            return sum;
+        }
+    }
+}
+
+    
---
+++ Calling-Methods-Assignment/Calling-Methods-Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to do math on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int userAdded = Math.Add(userNum);
+            int userSub = Math.Subtract(userNum);
+            int userMulti = Math.Multiply(userNum);
+
+            Console.WriteLine(userNum + " + 5 is " + userAdded);
+            Console.WriteLine(userNum + " - 5 is " + userSub);
+            Console.WriteLine(userNum + " * 5 is " + userMulti);
+
+            Console.ReadLine();
+        }
+    }
+}
