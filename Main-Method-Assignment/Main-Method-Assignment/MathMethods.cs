b45588423d1e5cddad1e418e3b4af0ecd5e49022
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-15 11:07:50 -0400

    Main Method Assignment

---
+++ Main-Method-Assignment/Main-Method-Assignment/MathMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class MathMethods
+    {
+        public static int Add(int num)
+        {
+            return num + 5;
+        }
+
+        public static decimal Add(decimal num)
+        {
+            decimal sum = num + 4;
+            return Convert.ToInt32(sum);
+        }
+
+        public static int Add(string num)
+        {
+            int sum = Convert.ToInt32(num) + 7;
+            return sum;
+        }
+    }
+}
---
+++ Main-Method-Assignment/Main-Method-Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            decimal num2 = 2.7m;
+            string num3 = "8";
+
+            Console.WriteLine(MathMethods.Add(num));
+            Console.WriteLine(MathMethods.Add(num2));
+            Console.WriteLine(MathMethods.Add(num3));
+
+            Console.ReadLine();
+        }
+    }
+}
