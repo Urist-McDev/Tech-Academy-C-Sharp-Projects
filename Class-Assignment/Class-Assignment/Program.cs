b443b5ccdec7b2a308420af0ae550d63d0bde8e9
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-15 12:37:00 -0400

    Class assignment

---
+++ Class-Assignment/Class-Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            voidStuff.Math(userNum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class-Assignment/Class-Assignment/voidStuff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    class voidStuff
+    {
+        public static void Math(int num)
+        {
+            Console.WriteLine(num / 2);
+        }
+
+        public static void Math(int num, out int numUsed)
+        {
+            Console.WriteLine(num / 2);
+
+            numUsed = num;
+        }
+    }
+}
