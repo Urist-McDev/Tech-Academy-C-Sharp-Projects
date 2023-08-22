4eb98e9eeccc61913f774440b49d0f2b95074208
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-08-22 10:37:07 -0400

    Datetime assignment

---
+++ Datetime-assignment/Datetime-assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            TimeSpan time = new TimeSpan(userNum, 0, 0);
+
+            Console.WriteLine(DateTime.Now + time);
+
+            Console.ReadLine();
+        }
+    }
+}
